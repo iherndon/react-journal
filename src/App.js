37cd9a809aedd4c2a16cbb4c2367aed321630736
Ivan <ivan@macbook-pro.lan>
2021-05-05 19:06:30 -0400

    journal and journal entries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [journalEntry, setJournalEntry] = useState("");
+  const [entries, setEntries] = useState([]);
+  const [visibleJournalEntry, setVisibleJournalEntry] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>My Journal</h1>
+      <textarea
+        placeholder="Click here to start typing..."
+        value={journalEntry}
+        onChange={function (evt) {
+          setJournalEntry(evt.target.value);
+        }}
+      />
+      <button
+        id="submit"
+        onClick={function () {
+          setEntries([...entries, journalEntry]);
+          setJournalEntry("");
+        }}
+      >
+        Submit
+      </button>
+      <section id="entries">
+        <div id="entries-nav">
+          {entries.map(function (entry, index) {
+            return (
+              <button
+                className={entry === visibleJournalEntry ? "selected" : ""}
+                key={index}
+                onClick={function () {
+                  setVisibleJournalEntry(entry);
+                }}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+        </div>
+        <p id="single-entry">{visibleJournalEntry}</p>
+      </section>
+    </main>
   );
 }
 
